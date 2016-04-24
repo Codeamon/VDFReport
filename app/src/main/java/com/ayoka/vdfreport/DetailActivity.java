d03ca0562f05ae4b5e3c93dfa4196849231f6b29
deuahmet <ahmetyildirim3@gmail.com>
2016-04-24 14:04:23 +0300

    committed

---
+++ app/src/main/java/com/ayoka/vdfreport/DetailActivity.java
@@ -0,0 +1,16 @@
+package com.ayoka.vdfreport;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ahmetyildirim on 23.4.2016.
+ */
+public class DetailActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
