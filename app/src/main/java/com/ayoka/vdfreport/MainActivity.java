2fb25a3c21a5addae77ee9fad231706791364f56
deuahmet <ahmetyildirim3@gmail.com>
2016-04-26 22:57:09 +0300

    logi ekranı geri buton kontrolü giriş ve menü de ekran sabitleme

--- app/src/main/java/com/ayoka/vdfreport/MainActivity.java
+++ app/src/main/java/com/ayoka/vdfreport/MainActivity.java
@@ -2,14 +2,18 @@ package com.ayoka.vdfreport;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,6 +34,40 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+        if (keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0) {
+
+            try {
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this); //Mesaj Penceresini Yaratalım
+
+                alertDialogBuilder.setTitle("Programdan Çıkılsın Mı?").setCancelable(false).setPositiveButton("Evet", new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) { //Eğer evet butonuna basılırsa
+                        dialog.dismiss();
+                        Intent intent = new Intent(Intent.ACTION_MAIN);
+                        intent.addCategory(Intent.CATEGORY_HOME);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(intent);
+                    }
+                }).setNegativeButton("Hayır", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getApplicationContext(), "Programdan çıkmaktan vazgeçtiniz.", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                alertDialogBuilder.create().show();
+                return super.onKeyDown(keyCode, event);
+            } catch (IllegalStateException e) {  //yapımızı try-catch blogu içerisine aldık
+                e.printStackTrace();
+            }
+        }
+        return true;
+    }
+
     private void hide(final View view) {
 
         int cx = (view.getLeft() + view.getRight()) / 2;
