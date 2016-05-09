f962ae13c5f13f8cfa3bd4acdbf0d7a17f8e0f83
deuahmet <ahmetyildirim3@gmail.com>
2016-05-09 10:20:43 +0300

    commit

--- app/src/main/java/com/ayoka/Adapters/ReportPagerAdapter.java
+++ app/src/main/java/com/ayoka/Adapters/ReportPagerAdapter.java
@@ -5,6 +5,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 
 import com.ayoka.Model.ReportDetail;
+import com.ayoka.fragments.ReportFragment;
 import com.ayoka.fragments.ReportInsComDailyTabFragments;
 import com.ayoka.fragments.ReportTab1Fragments;
 import com.ayoka.fragments.ReportTab2Fragments;
@@ -12,7 +13,7 @@ import com.ayoka.fragments.ReportTab2Fragments;
 public class ReportPagerAdapter extends FragmentStatePagerAdapter {
 	int mNumOfTabs;
 	ReportDetail reportDetail;
-	public ReportTab1Fragments tab1;
+	public ReportFragment tab1;
 	public ReportPagerAdapter(FragmentManager fm, int NumOfTabs, ReportDetail reportDetail) {
 		super(fm);
 		this.mNumOfTabs = NumOfTabs;
@@ -22,18 +23,11 @@ public class ReportPagerAdapter extends FragmentStatePagerAdapter {
 	@Override
 	public Fragment getItem(int position) {
 
-		switch (position) {
-			case 0:
-				tab1 = new ReportTab1Fragments();
+		tab1 = new ReportFragment();
+		tab1.reportList=this.reportDetail.getReportList().get(position);
+		tab1.reportType=1;
+		return tab1;
 
-				return tab1;
-			case 1:
-				ReportTab2Fragments tab2 = new ReportTab2Fragments();
-				tab2.reportList=this.reportDetail.getReportList().get(1);
-				return tab2;
-			default:
-				return null;
-		}
 	}
 
 	@Override
